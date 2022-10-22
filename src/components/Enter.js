bc8b052fb899590b8d1aeb2d4ec8b0196b3d1b14
Devign Experience <devignx@gmail.com>
2022-10-22 18:54:21 +0530

    Add files via upload

--- src/components/Enter.js
+++ src/components/Enter.js
@@ -1,6 +1,7 @@
 import React from "react";
 import '../styles/Enter.css';
 import { motion } from "framer-motion";
+import Enterimg from '../assets/enter.svg'
 
 const Enter = ({ handleEnter }) => {
     return(
@@ -10,8 +11,10 @@ const Enter = ({ handleEnter }) => {
                 scale:1.04,
                 cursor: 'pointer'
             }}
-            className="enter w-20 h-20 rounded-full border-black p-8 flex items-center justify-center">
-            <p className="text-xs text-white">ENTER</p>
+
+            className="enter w-100 h-20 rounded-full border-black p-10 flex items-center justify-center">
+            <p className="text-sm text-white">ENTER</p>
+            <img className="pl-1 w-6" src={Enterimg}></img>
         </motion.div>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -38,15 +38,23 @@ const Home = () => {
         .catch(()=>console.log("error getting public ip address"))
     }, [])
 
+
+
     return(
         <div className="h-screen">
             <Nav/>
-            <div className="w-full h-3/5 flex items-center justify-center">
-                <Enter handleEnter = {handleEnter}/>
+            <div className="w-full  h-3/5 flex items-center justify-center flex-col gap-y-2">
+                <Enter autoFocus handleEnter = {handleEnter}/>
+                <p id="enterroom" className="hidden md:inline text-xs text-gray-400">or Press Enter ↵ for Chat</p>
+                <p id="enterroom" className="inline text-xs text-gray-400 md:hidden">Click to Chat</p> 
             </div>
-            <div className="w-full h-1/5 flex items-center flex-col gap-y-2 bg-pink-100 justify-center">
+            <div className="w-full h-1/7 flex items-center flex-col gap-y-2 bg-pink-100 justify-center md: h-1/5">
                 <p className="text-xs text-rose-500 font-semibold">NOTE</p>
-                <p className="text-xs font-thin">	• Connect all devices in the same wifi</p>
+                <p className="text-xs font-thin">	
+                • Connect all devices in the same wifi <br></br>
+                • Ask for Password from the organisation you are at :)<br></br>
+                • Chat rooms are unmoderated
+                </p>
             </div>
             {
                 enter && (
@@ -56,13 +64,13 @@ const Home = () => {
                          animate="visible"
                          className="absolute z-10 top-1/4 w-full h-3/4 bg-gray-900 flex items-center flex-col">
                          <img className="w-20 -translate-y-10"  src={connect}></img>    
-                            <div className="h-4/5 flex items-center justify-evenly flex-col gap-y-3">
+                            <div className="h-4/5 flex items-center flex-col gap-y-5 lg: gap-y-10">
                             <div className="text-center font-thin flex-col gap-y-0.5">
                                 <p className="text-xs text-gray-500">Before entering</p>
                                 <p className="text-base text-gray-50">Give yourself a cool Username</p>
                             </div>
                             <form className="flex items-center justify-evenly flex-col gap-y-4">
-                                    <input autoFocus className="rounded border-0 outline-0 py-1.5 px-3 text-xs w-full" type="text" placeholder="John Doe"/>
+                                    <input autoFocus autoComplete="name" className="rounded border-0 outline-0 py-1.5 px-3 text-xs w-full" type="text" placeholder="John Doe"/>
                                     <button type="submit"onSubmit={()=> handleEnter() } className="rounded btn py-1.5 bg-white text-xs text-white border-0 outline-0 w-full">ENTER</button>
                             </form>
                             <p className="cursor-pointer font-thin text-gray-300 text-xs underline" onClick={()=> handleEnter()}>Go back</p>
